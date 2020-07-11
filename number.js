ecd18c862f725e8ca81f8e3e3d7f7d8887f2ce54
Arman Kamal <armankamal2421@gmail.com>
2020-07-11 08:09:17 -0700

    String & Number

---
+++ number.js
@@ -0,0 +1,21 @@
+//whole number 
+
+let radius = 10
+
+// decimal number 
+const pi = 3.14
+
+//math operators  + - * / ** %
+
+let num1 = 19
+let num2 = 15
+
+let add = num1+num2
+let sub = num1-num2
+let multi = num1*num2
+let divi = num1/num2
+let power = num1**num2
+let remainder = num1%num2
+
+console.log(add,sub,multi,divi,power,remainder)
+
---
+++ string.js
@@ -0,0 +1,22 @@
+//commin string methods
+
+let email = 'arman@coc.com'
+
+//Finding the last index of given character
+
+// let result = email.lastIndexOf('c')
+// console.log(result)
+
+// Slice the given string (From - to )
+
+// let result = email.slice(0,5)
+// console.log(result)
+
+//From where to how many characters 
+// let result = email.substr(0,5)
+// console.log(result)
+
+//Replace the first character with the given one 
+
+let result = email.replace('m','k')
+console.log(result)
