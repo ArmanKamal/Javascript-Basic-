bfa90b7f3ae2ac692df83baa8417e655f92be2bc
Arman Kamal <armankamal2421@gmail.com>
2020-07-27 06:47:35 -0700

    Added Type Conversion

---
+++ typeConversion.js
@@ -0,0 +1,22 @@
+let score = '100'
+
+console.log(score+1)
+
+score = Number(score)
+console.log(score+1)
+console.log(typeof score)
+
+let result1 = String(50)
+console.log(result1, typeof result1)
+
+result1 = Boolean(100)
+console.log(result1, typeof result1)
+
+result1 = Boolean(0)
+console.log(result1, typeof result1)
+
+result1 = Boolean('0')
+console.log(result1, typeof result1)
+
+result1 = Boolean('')
+console.log(result1, typeof result1)
